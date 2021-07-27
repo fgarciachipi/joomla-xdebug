25907772a327a4437d8c2b35667dc6be3419e032
PenyPip <penynikolaidou@gmail.com>
2021-07-27 14:57:17 +0300

    first commit on joomla-installation

---
+++ html/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-07-27 10:19:40 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-07-27T10:19:40+00:00	INFO 172.20.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
